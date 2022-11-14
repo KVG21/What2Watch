0c13e880f6994932287eec734e27a226fff898db
JunnoTo <t0juto00@students.oamk.fi>
2022-11-14 18:24:28 +0200

    homescreen hienosäätöä

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, onSnapshot, query, doc, getDocs } from "firebase/firestore";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 
-const firebaseConfig = { 
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+const firebaseConfig = {
+  apiKey: "AIzaSyBzPtsLylRN0r17C8XywaYVssgbB99XCQs",
+  authDomain: "what2watch-40e6b.firebaseapp.com",
+  projectId: "what2watch-40e6b",
+  storageBucket: "what2watch-40e6b.appspot.com",
+  messagingSenderId: "379782782889",
+  appId: "1:379782782889:web:09fb2e8adc39f36b5baeb8"
 };
 
 initializeApp(firebaseConfig);
