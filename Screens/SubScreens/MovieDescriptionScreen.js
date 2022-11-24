6af203dc16e154c47d656f9715495bdeba1a7438
JunnoTo <t0juto00@students.oamk.fi>
2022-11-25 01:40:29 +0200

    desc sivujen tyylittelyä
    viimeistellään kunhan trailerin toisto on kunnossa

--- Screens/SubScreens/MovieDescriptionScreen.js
+++ Screens/SubScreens/MovieDescriptionScreen.js
@@ -1,23 +1,46 @@
-import {View, Text} from 'react-native'
-import styles from '../../styles/login'
+import { View, Text} from 'react-native'
+import styles from '../../styles/descScreens'
+import Icon from "react-native-vector-icons/Ionicons";
 
 
 export default function MovieDescriptionScreen({route}) {
     const {item} = route.params;
+
   return (
     <>
     {item.map((item, key) => {
         return (
             <View style = { styles.container} key = {key}>
-                <Text style = {styles.buttonOutlineText}>Title : {item.Title}</Text>
-                <Text style = {styles.buttonOutlineText}>PgR : {item.PgR}</Text>
-                <Text style = {styles.buttonOutlineText}>Rating : {item.Rating}</Text>
-                <Text style = {styles.buttonOutlineText}>Stars : {item.Stars}</Text>
-                <Text style = {styles.buttonOutlineText}>Time : {item.Time}</Text>
-                <Text style = {styles.buttonOutlineText}>Trailer : {item.Trailer}</Text>
-                <Text style = {styles.buttonOutlineText}>Desc : {item.Description}</Text>
-                <Text style = {styles.buttonOutlineText}>Genre : {item.Genre}</Text>
-                <Text style = {styles.buttonOutlineText}>Director : {item.Director}</Text>
+                <Text style = {styles.title}>{item.Title}</Text>
+
+                <View style = { styles.timeCont}>
+                  <Icon name='time' style = {styles.icon}></Icon>
+                  <Text style = {styles.time}>{item.Time}</Text>
+                </View>
+
+                <Text style = {styles.descText}>Trailer : {item.Trailer}</Text>
+
+                <View style = {styles.iconRow}>
+                  <Icon name='eye' style = {styles.icon}></Icon>
+                  <Text style = {styles.descText}>{item.PgR}</Text>
+
+                  <Icon name='star' style={styles.icon}></Icon>
+                  <Text style = {styles.descText}>{item.Rating}</Text>
+
+                  <Icon name='albums' style = {styles.icon}></Icon>
+                  <Text style = {styles.descText}>{item.Genre}</Text>
+
+                  <Icon name='heart' style = {styles.icon}></Icon>
+                  <Text style = {styles.descText}>Add to list</Text>
+                </View>
+
+
+                <Text style = {styles.descDir}>Director : {item.Director}</Text>
+                <Text style = {styles.descStars}>Stars : {item.Stars}</Text>
+
+                <View style = { styles.descBox}>
+                <Text style = {styles.textDesc}>Desc : {item.Description}</Text>
+                </View>
             </View>
         )
     })}
---
+++ styles/descScreens.js
@@ -0,0 +1,67 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(20,20,20,1)'
+    },
+    title:{
+        color:'white',
+        fontSize:24,
+        marginTop: 80,
+    },
+    icon:{
+        color: "rgba(145,98,228,1)",
+        fontSize: 21,
+        marginTop: 4,
+    },
+    time:{
+        color:'white',
+        marginTop:5,
+        marginLeft:5,
+        fontSize:15
+    },
+    timeCont: {
+        flexDirection:'row',
+        alignItems: 'center',
+    },
+    descText:{
+        color: 'white',
+        fontSize: 12,
+    },
+    iconRow: {
+        flexDirection:'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        marginTop: 14,
+    },
+    descDir: {
+        color: 'white',
+        fontSize: 16,
+        marginTop: 35,
+        marginLeft:10,
+    },
+    descStars:{
+        color: 'white',
+        fontSize:16,
+        marginTop:28,
+        marginLeft:10,
+    },
+    descBox:{
+        width:312,
+        backgroundColor: "rgba(37,37,37,1)",
+        marginTop: 40,
+        marginLeft: 24,
+        paddingBottom:50,
+        paddingHorizontal:10,
+    },
+    textDesc: {
+        color: 'white',
+        marginTop:20,
+        marginLeft:5,
+    },
+    episodesCont: {
+        flexDirection:'row',
+        alignItems: 'center',
+    }
+})
