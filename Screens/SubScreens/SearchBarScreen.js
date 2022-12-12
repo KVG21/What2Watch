6c45a1d69b6945fb56b6b15387e83afc8ec5f3de
maijacita <92377463+maijacita@users.noreply.github.com>
2022-12-12 14:09:47 +0200

    screen

---
+++ Screens/SubScreens/SearchBarScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchBarScreen = () => {
+  return (
+    <View>
+      <Text>SearchBarScreen</Text>
+    </View>
+  )
+}
+
+export default SearchBarScreen
